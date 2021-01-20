0a39bd1ad8590f53a9585960167de7ba2eb7b06e
Jameson Hoang <hoangjam@sheridancollege.ca>
2021-01-20 15:18:17 -0500

    afgd

---
+++ testtest/src/main/java/com/example/demo/TesttestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesttestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesttestApplication.class, args);
+    }
+
+}
