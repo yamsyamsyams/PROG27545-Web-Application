a85697e7cb9030982371f2e43f8fca1bfb534663
Jameson Hoang <hoangjam@sheridancollege.ca>
2021-01-25 11:17:45 -0500

    week2

---
+++ Week2_StudentMVCApplication/src/main/java/ca/sheridancollege/hoangjam/Week2StudentMvcApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.hoangjam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2StudentMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week2StudentMvcApplication.class, args);
+    }
+
+}
---
+++ Week2_StudentMVCApplication/src/test/java/ca/sheridancollege/hoangjam/Week2StudentMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.hoangjam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2StudentMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
