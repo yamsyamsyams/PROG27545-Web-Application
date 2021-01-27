7d3a7e582b3e50a9f42a4588819507c95e4dcaa0
Jameson Hoang <hoangjam@sheridancollege.ca>
2021-01-27 13:02:42 -0500

    exercise2_1 - didn't use the platforms[] array

---
+++ Exercise2_1/src/test/java/ca/sheridancollege/hoangjam/Exercise21ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.hoangjam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
