cf6d096513e3948871756d5f8042aa0772e9778a
Jameson Hoang <hoangjam@sheridancollege.ca>
2021-02-07 00:40:12 -0500

    at least html works..

--- Assignment1_JamesonHoang/src/main/java/ca/sheridancollege/hoangjam/controllers/GameController.java
+++ Assignment1_JamesonHoang/src/main/java/ca/sheridancollege/hoangjam/controllers/GameController.java
@@ -2,19 +2,38 @@ package ca.sheridancollege.hoangjam.controllers;
 
 import ca.sheridancollege.hoangjam.model.Game;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GameController {
 
-    @RequestMapping("/")
-    public ModelAndView index(){
-        Game game = new Game();
+    String[] choiceNames = {
+            "Paper",
+            "Rock",
+            "Scissors"};
 
+    @RequestMapping("/")
+    public ModelAndView index(@RequestParam(defaultValue = "0") int choice){
+        Game game = new Game();
         ModelAndView modelAndView = new ModelAndView("input", "game", game);
+        modelAndView.addObject("userChoice", choice);
+        modelAndView.addObject("choiceNames", choiceNames);
 
         return modelAndView;
     }
+
+    @PostMapping("processInput")
+    public String processChoice(Model model, @ModelAttribute Game game){
+        model.addAttribute("choice", game.getUserChoice());
+
+
+        return "output";
+    }
+
 
 }
--- Assignment1_JamesonHoang/src/main/java/ca/sheridancollege/hoangjam/model/Game.java
+++ Assignment1_JamesonHoang/src/main/java/ca/sheridancollege/hoangjam/model/Game.java
@@ -2,17 +2,48 @@ package ca.sheridancollege.hoangjam.model;
 
 import lombok.Data;
 
-@Data
-public class Game {
-    private int userChoice;
-    private int comChoice;
+import java.io.Serializable;
 
-    private int rock = 0;
-    private int paper = 1;
+@Data
+public class Game implements Serializable {
+    private int rock = 1;
+    private int paper = 0;
     private int scissor = 2;
 
-    private String[] choiceNames = {"Rock", "Paper", "Scissor"};
+    // possible outcomes
+    private static int computer = 0;
+    private static int player = 1;
+    private static int draw = 2;
+
+    private int userChoice = rock;
+    private int comChoice;
+
+    private String[] choiceNames = {"Paper", "Rock", "Scissor"};
     public String[] winnerNames = {"Computer", "Player", "Draw"};
+
+    private static final int[][] winnerSelect =
+            {
+                    // computer selected paper
+                    {
+                            draw, // user selected paper
+                            computer, // user selected rock
+                            player // user selected scissors
+                    },
+
+                    // computer selected rock
+                    {
+                            player, // user selected paper
+                            draw, // user selected rock
+                            computer // user selected scissors
+                    },
+
+                    // computer selected scissors
+                    {
+                            computer, // user selected paper
+                            player, // user selected rock
+                            draw // user selected scissors
+                    }
+            };
 
     public Game() {
         comChoice = (int) (3 * Math.random());
@@ -34,6 +65,40 @@ public class Game {
         else{
             throw new IllegalArgumentException("Invalid computer choice");
         }
+    }
+
+    public String getUserChoiceName(){
+        return choiceNames[userChoice];
+    }
+
+    public String getComputerChoiceName(){
+        return choiceNames[comChoice];
+    }
+
+    // get the winner for hardcoded values
+    public int getWinner(){
+        return winnerSelect[comChoice][userChoice];
+    }
+
+    // provide winner value to get the winner name
+    public String getWinnerName(){
+        return winnerNames[getWinner()];
+    }
+
+    public void setComputerName(String computerName){
+        winnerNames[computer] = computerName;
+    }
+
+    public String getComputerName(){
+        return winnerNames[computer];
+    }
+
+    public void setUserName(String userName){
+        winnerNames[player] = userName;
+    }
+
+    public String getUserName(){
+        return winnerNames[player];
     }
 
 }
