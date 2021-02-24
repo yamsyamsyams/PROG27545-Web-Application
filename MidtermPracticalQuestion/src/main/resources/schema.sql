362545a0ca33821bb63342a058299ff374ee7e4f
Jameson Hoang <hoangjam@sheridancollege.ca>
2021-02-24 12:26:49 -0500

    first commit

---
+++ MidtermPracticalQuestion/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE fruit(
+	id  int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	fType  varchar(10) NOT NULL,
+    weight  int NOT NULL,
+    subTotal decimal  NOT NULL
+);
---
+++ MidtermPracticalQuestion/src/test/java/ca/sheridancollege/midterm/MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
