3a5740d4b2b40b03d8ad63b9fde68e3816f816dc
Jameson Hoang <hoangjam@sheridancollege.ca>
2021-03-24 22:39:22 -0400

    example

--- Assignment3_JamesonHoang/src/main/java/ca/sheridancollege/hoangjam/data/Contact.java
+++ Assignment3_JamesonHoang/src/main/java/ca/sheridancollege/hoangjam/data/Contact.java
@@ -2,8 +2,12 @@ package ca.sheridancollege.hoangjam.data;
 
 
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Table(name = "contact")
-public class Contact {
+public class Contact implements Serializable {
+
+    private Integer id;
+    private String fullName = "";
 
 }
