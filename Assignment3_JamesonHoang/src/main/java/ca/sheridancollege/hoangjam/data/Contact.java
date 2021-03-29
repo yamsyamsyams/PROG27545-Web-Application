cd225346488c266e7440474fa3ac3292ea872086
Jameson Hoang <hoangjam@sheridancollege.ca>
2021-03-29 11:12:16 -0400

    week 10 examples

--- Assignment3_JamesonHoang/src/main/java/ca/sheridancollege/hoangjam/data/Contact.java
+++ Assignment3_JamesonHoang/src/main/java/ca/sheridancollege/hoangjam/data/Contact.java
@@ -1,12 +1,63 @@
 package ca.sheridancollege.hoangjam.data;
 
 
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
-@Table(name = "contact")
+@Entity
+@Table(name = "contacts")
 public class Contact implements Serializable {
 
+    @Column(name="id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "full_name")
     private String fullName = "";
+    private String contactType = "";
+    private String phoneNumber = "";
+    private String address = "";
+    private String email = "";
+
+    public Contact(){
+    };
+
+    public Integer getId(){
+        return id;
+    }
+    public void setId(){
+        this.id = id;
+    }
+    public String getFullName(){
+        return fullName;
+    }
+    public void setFullName(){
+        this.fullName = fullName;
+    }
+    public String getContactType(){
+        return contactType;
+    }
+    public void setContactType(){
+        this.contactType = contactType;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public void setPhoneNumber(){
+        this.phoneNumber = phoneNumber;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public void setAddress(){
+        this.address = address;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(){
+        this.email = email;
+    }
+
 }
--- Assignment3_JamesonHoang/src/main/resources/schema.sql
+++ Assignment3_JamesonHoang/src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(70),
     contact_type VARCHAR(30),
-    phone_number INTEGER,
+    phone_number VARCHAR(20),
     address VARCHAR(100),
-    email VARCHAR(50),
+    email VARCHAR(50)
 );
